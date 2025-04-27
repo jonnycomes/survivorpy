758980ad641a584f7d1c0d15e8b6d948cfea25da
Jonny Comes <jcomes@collegeofidaho.edu>
2025-04-26 23:01:44 -0600

    Added docstring

--- survivorpy/__init__.py
+++ survivorpy/__init__.py
@@ -1,3 +1,21 @@
+"""
+SurvivorPy is a Python wrapper for the data provided by the R package 'survivoR'. 
+It allows users to easily access clean, structured data on *Survivor* contestants, 
+seasons, episodes, votes, and other related content, all through Python and in the 
+form of `pandas` DataFrames.
+
+Key Features:
+- Seamless integration with the R package 'survivoR', providing up-to-date data (with no more than a one-week delay).
+- Direct access to *Survivor* data tables via the `load()` function or direct table imports.
+- Efficient caching mechanism that allows for offline access to tables while syncing data with the source.
+- Includes attributes like `TABLE_NAMES` for listing available data tables and `LAST_SYNCED` for tracking data synchronization.
+
+Data is cached upon first access, and users can refresh their local cache to ensure they have the latest available data by calling the `refresh_data()` function.
+
+This module provides an easy and efficient way to interact with detailed *Survivor* data, designed to integrate smoothly with the Python data ecosystem.
+
+"""
+
 from .data import load, refresh_data, get_table_names, get_last_synced
 from .config import has_cache
 
