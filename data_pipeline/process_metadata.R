29e1344952e3d696e1ba29ea546d6c81b5b5d307
Jonny Comes <jcomes@collegeofidaho.edu>
2025-04-30 13:55:09 -0600

    Minor change to system environ var

--- data_pipeline/process_metadata.R
+++ data_pipeline/process_metadata.R
@@ -69,7 +69,7 @@ upload_last_updated <- function(metadata, sha = NULL) {
     url,
     body = body,
     encode = "json",
-    httr::authenticate(Sys.getenv("GITHUB_USER"), Sys.getenv("GH_PAT"))
+    httr::authenticate(Sys.getenv("GITHUB_USER"), Sys.getenv("GITHUB_PAT"))
   )
 
   if (httr::status_code(resp) >= 300) {
